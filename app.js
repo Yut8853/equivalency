0f866ae7cc071ebb467afc85dc707150242e1254
yk0611 <hello@junkbranding.com>
2023-03-16 11:47:24 +0900

    同値課題修正０１提出

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // ==と===の違いは何ですか？
 
-console.log('型の違いもわかる。')
-console.log('＝＝で1 == n\'1n\'場合、自動で数値型に変換し比較が行われる')
+console.log(`型の違いもわかる。
+＝＝で1 == n\'1n\'場合、自動で数値型に変換し比較が行われる`)
 console.log('ーーーーーーーーーーーーーーーーーーーーーーーーーーーー')
 
 // 1 == '1'の結果は何ですか？
-
-console.log('false')
-console.log('なぜなら左右の型が違うから。左は数値型で右は文字列型')
+// const result = 1 == '1';
+// if (result === false) console.log(`PASS`);
+// else console.error(`FAIL`);
+console.log('true（でした）')
+console.log('なぜなら比較する前に両方の値が同じ型になるから')
 console.log('ーーーーーーーーーーーーーーーーーーーーーーーーーーーー')
 
 // 1 === '1'の結果は何ですか？
@@ -39,5 +41,16 @@ console.log('ーーーーーーーーーーーーーーーーーーーーーー�
 console.log('false')
 console.log('左がundefinedだから')
 console.log('ーーーーーーーーーーーーーーーーーーーーーーーーーーーー')
+
+// undefinedとnullの違い
+console.log(`undefinedとnullの違いは基本的には似ているが違いとして
+参照先がないときなどに返されるときは自動的にundefinedが返ってくるがnullは割り当てる必要がある。
+これでエラーとか少しわかるようになった気がします`);
+
+let variable;
+console.log(variable); // undefined
+
+let variable2 = null;
+console.log(variable2); // null
 
 
